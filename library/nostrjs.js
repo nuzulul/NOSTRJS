183e1bfa857ef3885e6381eb57b78010e7e637ea
nuzulul <nuzulz@gmail.com>
2024-07-31 12:04:31 +0700

    feat : add dial protocol

--- library/nostrjs.js
+++ library/nostrjs.js
@@ -1,5 +1,10 @@
 import {
-	mkErr
+	mkErr,
+	pipe,
+	lp,
+	map,
+	uint8ArrayToString,
+	uint8ArrayFromString
 } from './utils'
 import * as constants from  './constants'
 import {webpeerjs} from 'webpeerjs'
@@ -10,14 +15,29 @@ class nostrjs{
 	webpeer
 	online
 	
+	#nostrjsDial
+	#nostrjsRun
+	
 	constructor(webpeer){
 		
 		this.webpeer = webpeer
 		this.online = 0
 		
+		this.#nostrjsDial =  new Map()
+		this.#nostrjsRun = false
+		
 		this.webpeer.IPFS.libp2p.addEventListener('peer:identify', async (evt) => {
 			if(evt.detail.protocols.includes(constants.CONSTANTS_PROTOCOL) && !evt.detail.connection.transient){
-				console.log('peer:identify '+evt.detail.peerId.toString(),evt)
+				//console.log('peer:identify '+evt.detail.peerId.toString(),evt)
+				//console.log('peer:identify '+evt.detail.peerId.toString(),evt.detail.observedAddr.toString())
+				if(!this.#nostrjsDial.has(evt.detail.peerId.toString())){
+					this.#nostrjsDial.set(evt.detail.peerId.toString(),evt.detail.peerId)
+					if(!this.#nostrjsRun){
+						this.#nostrjsRun = true
+						console.log('runqueue')
+						this.#nostrjsQueue()
+					}
+				}
 			}
 		})
 		
@@ -34,8 +54,32 @@ class nostrjs{
 	async #registerProtocol(){
 		
 		const handler = async ({ connection, stream }) => {
+			const message = 'sync'
 			const id = connection.remotePeer.toString()
 			console.log('handler',id)
+			try{
+				const output = await pipe(
+					stream.source,
+					(source) => lp.decode(source),
+					(source) => map(source, (buf) => uint8ArrayToString(buf.subarray())),
+					async function (source) {
+					  let string = ''
+					  for await (const msg of source) {
+						string += msg.toString()
+					  }
+					  return string
+					}
+				)
+				//console.log('outputhandler',output)
+				pipe(
+					message,
+					(source) => map(source, (string) => uint8ArrayFromString(string)),
+					(source) => lp.encode(source),
+					stream.sink
+				)
+			}catch(err){
+				console.log('err',err)
+			}
 		}
 
 		await this.webpeer.IPFS.libp2p.handle(constants.CONSTANTS_PROTOCOL, handler, {
@@ -46,7 +90,49 @@ class nostrjs{
 
 	}
 	
-	static async createNostrDB(){
+	#nostrjsQueue(){
+		for(const nostrjs of this.#nostrjsDial){
+			const id = nostrjs[0]
+			const peersId = nostrjs[1]
+			if(peersId){
+				this.#nostrjsDial.set(id,false)
+				console.log('dialprotocol')
+				this.#nostrjsDialProtocol(peersId)
+				return
+			}
+		}
+		this.#nostrjsRun = false
+	}
+	
+	async #nostrjsDialProtocol(peersId){
+		console.log('dial',peersId.toString())
+		const message = 'sync'
+		try{
+			const stream = await this.webpeer.IPFS.libp2p.dialProtocol(peersId, constants.CONSTANTS_PROTOCOL,{runOnTransientConnection:false})
+			const output = await pipe(
+				message,
+				(source) => map(source, (string) => uint8ArrayFromString(string)),
+				(source) => lp.encode(source),
+				stream,
+				(source) => lp.decode(source),
+				(source) => map(source, (buf) => uint8ArrayToString(buf.subarray())),
+				async function (source) {
+				  let string = ''
+				  for await (const msg of source) {
+					string += msg.toString()
+				  }
+				  return string
+				}
+			)
+			console.log('outputdial',output)
+		}catch(err){
+			console.log('err',err)
+		}
+		console.log('this.#nostrjsDialed',this.#nostrjsDial)
+		this.#nostrjsQueue()
+	}
+	
+	static async createNostrClient(){
 		
 		let config = {}
 		
--- library/utils.js
+++ library/utils.js
@@ -1,5 +1,24 @@
 import * as constants from  './constants'
+import { pipe } from 'it-pipe'
+import * as lp from 'it-length-prefixed'
+import map from 'it-map'
 
 const prefix = constants.CONSTANTS_PREFIX
 
 export const mkErr = msg => new Error(`${prefix}: ${msg}`)
+
+export function uint8ArrayToString(uint8Array){
+	const string = new TextDecoder().decode(uint8Array)
+	return string
+}
+
+export function uint8ArrayFromString(string){
+	const uint8Array = new TextEncoder().encode(string)
+	return uint8Array
+}
+
+export { pipe }
+
+export { lp }
+
+export { map }
